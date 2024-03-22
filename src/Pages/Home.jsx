1c038244cc54ca01b41d319c61cc5ddd0faf6592
huntermahmud <mahmudthedatahunter@gmail.com>
2024-03-22 17:47:30 +0600

    navbar added with light dark theme functionality with localstorage save added

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../Components/Navbar";
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar />
+            <h1>home page</h1>
+        </div>
+    );
+};
+
+export default Home;
