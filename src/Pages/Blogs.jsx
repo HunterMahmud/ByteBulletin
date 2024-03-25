9dc8942c2caf3771e91d49819ebc071c29d20282
huntermahmud <mahmudthedatahunter@gmail.com>
2024-03-25 15:50:24 +0600

    netlify page refresh problem solved

--- src/Pages/Blogs.jsx
+++ src/Pages/Blogs.jsx
@@ -1,6 +1,7 @@
 import { Link, useLoaderData, useNavigation } from "react-router-dom";
 import Blog from "../Components/Blog";
 import Loading from './../Components/Loading';
+import imageNotFound from '../assets/404.jpg'
 
 
 
@@ -12,7 +13,7 @@ const Blogs = () => {
     <section className=" mx-auto">
 	<div className="container max-w-7xl p-6 mx-auto space-y-6 sm:space-y-12">
 		<Link rel="noopener noreferrer" to={`/blogs/${blogsData[0].id}`} className="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 ">
-			<img src={blogsData[0].cover_image} alt="" className="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 " />
+			<img src={blogsData[0].cover_image || imageNotFound} alt="" className="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 " />
 			<div className="p-6 space-y-2 lg:col-span-5">
 				<h3 className="text-2xl font-semibold sm:text-4xl group-hover:underline group-focus:underline">{blogsData[0].title}</h3>
 				<span className="text-xs ">{new Date(blogsData[0].published_at).toLocaleDateString("en-US", {
